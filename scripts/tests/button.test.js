74a5b28497b1c5656d6b2e645d5e0358a6b85045
Desislava Naydenova <deskata.01@gmail.com>
2024-05-28 10:28:30 +0000

    Test output of Java

---
+++ scripts/tests/button.test.js
@@ -0,0 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const buttonClick = require("../button");
+
+
+beforeEach(() => {
+    let fs = require("fs");
+    let fileContetnts = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContetnts);
+    document.close();
+});
+
+describe("DOM tests", ()=> {
+    test("exxpects p contetnt to change", ()=> {
+        buttonClick();
+        expect(document.getElementById("par").innerHTML).toEqual("You Clicked")
+    });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
+    });
+})
